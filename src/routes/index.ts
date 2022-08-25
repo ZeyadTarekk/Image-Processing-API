5edf8500bb1ce7eebef980295e8c935e1d2badc0
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-08-25 14:18:00 +0200

    add error message if a paramter is missing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,11 @@ import resize from "../util/resize";
 import mainPath from "../../util/path";
 const fs = require("fs");
 router.get("/images", async (req, res, next) => {
+  if (!req.query.width || !req.query.height || !req.query.filename) {
+    res.send("Missing Paramter! Enter all the three parameters");
+    return;
+  }
+
   if (
     !fs.existsSync(
       path.join(mainPath, "assets", "full", `${req.query.filename}.jpg`)
@@ -19,8 +24,20 @@ router.get("/images", async (req, res, next) => {
     Number(req.query.height),
     req.query.filename
   );
+
   res.send(
-    `<img src='/assets/thumb/${req.query.filename}${req.query.width}x${req.query.height}.jpg' alt='img'>`
+    `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    <body>
+    <img src='/assets/thumb/${req.query.filename}${req.query.width}x${req.query.height}.jpg' alt='img'>
+    </body>
+    </html>`
   );
 });
 
