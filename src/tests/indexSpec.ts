728eb5f98b13f99af648acdbb964b7f4bc5b7de2
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-08-25 15:08:34 +0200

    adding more unit tests

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -3,6 +3,14 @@ import app from "../index";
 
 const request = supertest(app);
 describe("Test endpoint responses", () => {
+  it("Test get / page", async () => {
+    const response = await request.get("/");
+    expect(response.status).toBe(404);
+  });
+  it("Test get /api page", async () => {
+    const response = await request.get("/api");
+    expect(response.status).toBe(404);
+  });
   it("Test wrong image name", async (done: DoneFn) => {
     const response = await request.get(
       "/api/images?filename=wrong&width=540&height=600"
