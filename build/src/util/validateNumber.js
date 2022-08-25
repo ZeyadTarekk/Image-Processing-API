30cdf346655af0755fed0d73f4f81eaea92d69bb
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-08-25 22:48:11 +0200

    remove build version

--- build/src/util/validateNumber.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var validateNumber = function (dim) {
-    return /^\d*$/.test(dim);
-};
-exports.default = validateNumber;
--- build/util/path.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var path = require("path");
-var main = require("require-main-filename")();
-var mainPath = path.join(path.dirname(main), "..");
-exports.default = mainPath;
